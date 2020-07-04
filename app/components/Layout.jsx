3417070f5ab55e18e754912b4801eab54d9dd180
Jalaj <Jalaj@mail.com>
2020-07-04 21:51:58 +0530

    Update Layout.jsx

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -3,7 +3,7 @@ import Container from './Container';
 
 const Layout = (props) => {
 	useEffect(() => {
-		document.title = `${props.title} | Complex App`;
+		document.title = `${props.title} | Write & Learn`;
 		window.scrollTo(0, 0);
 	}, [props.title]);
 	return (
