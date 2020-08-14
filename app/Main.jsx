e2a31a4f60c2bebda6e357f98f3d2ae32d588e14
Jalaj <Jalaj@mail.com>
2020-08-14 10:34:40 +0530

    Update Main.jsx

--- app/Main.jsx
+++ app/Main.jsx
@@ -26,7 +26,7 @@ const ViewSinglePost = React.lazy(() => import('./components/ViewSinglePost'));
 const Search = React.lazy(() => import('./components/Search'));
 const Chat = React.lazy(() => import('./components/Chat'));
 
-Axios.defaults.baseURL = process.env.BACKENDURL || 'https://backend-api-complex.herokuapp.com';
+Axios.defaults.baseURL = process.env.BACKENDURL || 'https://writeandlearn-api.herokuapp.com';
 
 function Main() {
 	const initialState = {
